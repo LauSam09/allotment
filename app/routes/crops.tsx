04c397b87fbea58220e69782ea6d8cf343c39789
Laurence Ede <laurence.ede@gmail.com>
2022-06-03 17:53:40 +0100

    Remove notes and splash UI

--- app/routes/crops.tsx
+++ app/routes/crops.tsx
@@ -2,7 +2,6 @@ import type { LoaderFunction } from "@remix-run/node";
 import { Form, Link, Outlet } from "@remix-run/react";
 
 import { requireUserId } from "~/session.server";
-import { useUser } from "~/utils";
 
 export const loader: LoaderFunction = async ({ request }) => {
   await requireUserId(request);
@@ -10,19 +9,16 @@ export const loader: LoaderFunction = async ({ request }) => {
 };
 
 export default function CropsPage() {
-  const user = useUser();
-
   return (
     <div className="flex h-full min-h-screen flex-col">
-      <header className="flex items-center justify-between bg-slate-800 p-4 text-white">
+      <header className="flex items-center justify-between bg-green-800 p-4 text-white">
         <h1 className="text-3xl font-bold">
-          <Link to=".">Crops</Link>
+          <Link to=".">ALLOTMENT</Link>
         </h1>
-        <p>{user.email}</p>
         <Form action="/logout" method="post">
           <button
             type="submit"
-            className="rounded bg-slate-600 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
+            className="rounded bg-green-600 py-2 px-4 text-green-100 hover:bg-green-500 active:bg-blue-600"
           >
             Logout
           </button>
